6cd74bb8deb55baf6a3000283c906f95e8635ea8
Grinch888 <48275812+Grinch888@users.noreply.github.com>
2019-03-12 17:34:54 +0300

    Data analysis lab with comments

--- Lab1.2/Data_Analysis.py
+++ Lab1.2/Data_Analysis.py
@@ -1,15 +1,19 @@
-from openpyxl import load_workbook
-from matplotlib import pyplot
+from openpyxl import load_workbook #Библиотека для работы с XLSX
+from matplotlib import pyplot #Библиотека построения графов
 
+#Загружаем файл XLSX в память
 wb = load_workbook('./data_analysis_lab.xlsx')
 sheet = wb['Data']
-sheet['A'][1:]
 
-def getvalue(x): return x.value
+#Читаем данные из ячеек
+
+def getvalue(x): return x.value #Функция получения значений из ячеек
+#Создаем списки значений из столбцов
 list_x = list(map(getvalue, sheet['A'][1:]))
 list_y = list(map(getvalue, sheet['D'][1:]))
 list_z = list(map(getvalue, sheet['C'][1:]))
 
+#Рисуем граф
 pyplot.plot(list_x, list_y, label="relationship")
 pyplot.plot(list_x, list_z, label="activity")
 pyplot.show()
